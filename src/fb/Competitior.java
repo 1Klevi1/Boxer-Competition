62829d516c7133bfb9142b897badefd23da89403
Klevi Alliu <ka21abv@herts.ac.uk>
2023-11-10 19:49:13 +0000

    created a constructor and getOverallScore method

--- src/fb/Competitior.java
+++ src/fb/Competitior.java
@@ -9,4 +9,14 @@ public class Competitior {
     private Level CompetitorLvl;
     private ArrayList<Integer> Scores;
     private int CompetitorCategory;
+
+    public Competitior(int competitorId, Person competitorDetails, Level competitorLvl, int competitorCategory) {
+        CompetitorId = competitorId;
+        CompetitorDetails = competitorDetails;
+        CompetitorLvl = competitorLvl;
+        CompetitorCategory = competitorCategory;
+    }
+    public double getOverallScore() { return 5; }
+
+
 }
