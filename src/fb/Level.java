8bbc7dd14177f06d3637365d30598aa828e73787
Klevi Alliu <ka21abv@herts.ac.uk>
2023-11-10 19:41:11 +0000

    adding Level enum

--- src/fb/Level.java
+++ src/fb/Level.java
@@ -1,2 +1,16 @@
-package fb;public enum Level {
+package fb;
+
+public enum Level {
+    NOVICE("Novice"), STANDARD("Standard"), VETERAN("Veteran");
+    private final String level;
+
+
+    Level(String typ) {
+        level = typ;
+    }
+
+
+    public String toString() {
+        return level;
+    }
 }
