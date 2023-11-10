0feee2c72c9245fbc3fa99af5f2a3f888596f108
Klevi Alliu <ka21abv@herts.ac.uk>
2023-11-10 19:30:59 +0000

    finished with the Person Class

--- src/fb/Person.java
+++ src/fb/Person.java
@@ -3,5 +3,32 @@ package fb;
 public class Person {
 
     private String Name;
+    private String middleName;
+    private String Surname;
+    private String dateOfBirth;
 
+    public Person(String name, String middleName, String surname, String dateOfBirth) {
+        Name = name;
+        this.middleName = middleName;
+        Surname = surname;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "Name='" + Name + '\'' +
+                ", middleName='" + middleName + '\'' +
+                ", Surname='" + Surname + '\'' +
+                ", dateOfBirth='" + dateOfBirth + '\'' +
+                '}';
+    }
 }
